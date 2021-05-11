d4fff7dbe5e4543944eb27fd3dbd67c13a41015e
spytni1212 <avdeenkoartem1212@gmail.com>
2021-05-12 01:42:24 +0300

    feat: add styles for meal items

--- components/CategoryGridTile.js
+++ components/CategoryGridTile.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { View, Text, StyleSheet, Button, FlatList, TouchableOpacity, Platform, TouchableNativeFeedback } from 'react-native'
+import { View, Text, StyleSheet, TouchableOpacity, Platform, TouchableNativeFeedback } from 'react-native'
 
 const CategoryGridTile = props => {
     let TouchableCmp = TouchableOpacity;
--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,23 +1,37 @@
 import React from 'react'
-import { View, Text, StyleSheet, Button } from 'react-native'
+import { View, Text, StyleSheet, FlatList } from 'react-native'
 
-import { CATEGORIES } from '../data/dummy-data'
+import { CATEGORIES, MEALS } from '../data/dummy-data'
+import MealItem from '../components/MealItem'
 
 const CategoryMealsScreen = props => {
+    const renderMealItem = itemData => {
+        return (
+            <MealItem 
+                title={itemData.item.title}
+                image={itemData.item.imageUrl}
+                duration={itemData.item.duration}
+                complexity={itemData.item.complexity}
+                affordability={itemData.item.affordability}
+                onSelectMeal={() => {
+
+                }}
+            />
+        )
+    }
+
     const categoryId = props.navigation.getParam('categoryId')
 
-    const selectedCategory = CATEGORIES.find(category => category.id === categoryId)
+    const displayedMeals = MEALS.filter(meal => meal.categoryIds.indexOf(categoryId) >= 0)
 
     return (
         <View style={styles.screen}>
-            <Text>The Category Meal Screen</Text>
-            <Text>{selectedCategory.title}</Text>
-            <Button title='go to meal detail' onPress={()=> {
-                props.navigation.navigate('MealDetail')
-            }}/>
-            <Button title='go back' onPress={()=> {
-                props.navigation.goBack()
-            }}/>
+            <FlatList 
+                data={displayedMeals}
+                keyExtractor={item => item.id}
+                renderItem={renderMealItem}
+                style={{width: '100%'}}
+            />
         </View>
     )
 }
