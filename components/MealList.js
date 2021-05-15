e7a5435ab4087643120439842b91ef0115818aa1
spytni1212 <avdeenkoartem1212@gmail.com>
2021-05-15 21:42:06 +0300

    feat: add switch favorite icon

--- components/MealList.js
+++ components/MealList.js
@@ -1,46 +1,56 @@
 import React from 'react'
 import { View, FlatList, StyleSheet } from 'react-native'
+import { useSelector } from 'react-redux'
 
 import MealItem from './MealItem'
 
-const MealList = props => {
-    const renderMealItem = itemData => {
-        return (
-            <MealItem 
-                title={itemData.item.title}
-                image={itemData.item.imageUrl}
-                duration={itemData.item.duration}
-                complexity={itemData.item.complexity}
-                affordability={itemData.item.affordability}
-                onSelectMeal={() => {
-                    props.navigation.navigate({routeName: 'MealDetail', params: {
-                        mealId: itemData.item.id,
-                        mealTitle: itemData.item.title
-                    }})
-                }}
-            />
-        )
-    }
+const MealList = (props) => {
+	const favoriteMeals = useSelector((state) => state.meals.favoriteMeals)
 
-    return (
-        <View style={styles.list}>
-            <FlatList 
-                data={props.listData}
-                keyExtractor={item => item.id}
-                renderItem={renderMealItem}
-                style={{width: '100%'}}
-            />
-        </View>
-    )
+	const renderMealItem = (itemData) => {
+		const isFavorite = favoriteMeals.some(
+			(meal) => meal.id === itemData.item.id
+		)
+		return (
+			<MealItem
+				title={itemData.item.title}
+				image={itemData.item.imageUrl}
+				duration={itemData.item.duration}
+				complexity={itemData.item.complexity}
+				affordability={itemData.item.affordability}
+				onSelectMeal={() => {
+					props.navigation.navigate({
+						routeName: 'MealDetail',
+						params: {
+							mealId: itemData.item.id,
+							mealTitle: itemData.item.title,
+							isFav: isFavorite,
+						},
+					})
+				}}
+			/>
+		)
+	}
+
+	return (
+		<View style={styles.list}>
+			<FlatList
+				data={props.listData}
+				keyExtractor={(item) => item.id}
+				renderItem={renderMealItem}
+				style={{ width: '100%' }}
+			/>
+		</View>
+	)
 }
 
 const styles = StyleSheet.create({
-    list: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
+	list: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
 })
 
 export default MealList
