05a335303693700e89923c0937379ccd2811cb72
spytni1212 <avdeenkoartem1212@gmail.com>
2021-05-15 22:04:32 +0300

    feat: add filtering logic

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -3,28 +3,47 @@ import { useSelector } from 'react-redux'
 
 import { CATEGORIES } from '../data/dummy-data'
 import MealList from '../components/MealList'
+import { View, StyleSheet } from 'react-native'
+import DefaultText from '../components/DefaultText'
 
-const CategoryMealsScreen = props => {
-    
-    const categoryId = props.navigation.getParam('categoryId')
+const CategoryMealsScreen = (props) => {
+	const categoryId = props.navigation.getParam('categoryId')
 
-    const availableMeals = useSelector(state => state.meals.filteredMeals)
+	const availableMeals = useSelector((state) => state.meals.filteredMeals)
 
-    const displayedMeals = availableMeals.filter(meal => meal.categoryIds.indexOf(categoryId) >= 0)
+	const displayedMeals = availableMeals.filter(
+		(meal) => meal.categoryIds.indexOf(categoryId) >= 0
+	)
 
-    return (
-        <MealList listData={displayedMeals} navigation={props.navigation} />
-    )
+	if (displayedMeals.length === 0) {
+		return (
+			<View style={styles.content}>
+				<DefaultText> No meals found, maybe check your filters</DefaultText>
+			</View>
+		)
+	}
+
+	return <MealList listData={displayedMeals} navigation={props.navigation} />
 }
 
 CategoryMealsScreen.navigationOptions = (navigationData) => {
-    const categoryId = navigationData.navigation.getParam('categoryId')
+	const categoryId = navigationData.navigation.getParam('categoryId')
 
-    const selectedCategory = CATEGORIES.find(category => category.id === categoryId)
+	const selectedCategory = CATEGORIES.find(
+		(category) => category.id === categoryId
+	)
 
-    return {
-        headerTitle: selectedCategory.title,
-    }
+	return {
+		headerTitle: selectedCategory.title,
+	}
 }
 
-export default CategoryMealsScreen;
+const styles = StyleSheet.create({
+	content: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
+})
+
+export default CategoryMealsScreen
--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -5,30 +5,46 @@ import { HeaderButtons, Item } from 'react-navigation-header-buttons'
 import MealList from '../components/MealList'
 import HeaderButton from '../components/HeaderButton'
 import mealsReducer from '../store/reducers/meals'
+import { View, Text, StyleSheet } from 'react-native'
+import DefaultText from '../components/DefaultText'
 
-const FavoritesScreen = props => {
-    const favMeals = useSelector(state => state.meals.favoriteMeals)
+const FavoritesScreen = (props) => {
+	const favMeals = useSelector((state) => state.meals.favoriteMeals)
 
-    return (
-        <MealList listData={favMeals} navigation={props.navigation}/>
-    )
+	if (favMeals.length === 0 || !favMeals) {
+		return (
+			<View style={styles.content}>
+				<DefaultText> No favorite meals found. Start adding some!</DefaultText>
+			</View>
+		)
+	}
+
+	return <MealList listData={favMeals} navigation={props.navigation} />
 }
 
-FavoritesScreen.navigationOptions = navData => {
-    return {
-        headerTitle: 'Your Favorites',
-        headerLeft: () => (
-            <HeaderButtons HeaderButtonComponent={HeaderButton}>
-                <Item 
-                    title='Menu' 
-                    iconName='ios-menu' 
-                    onPress={()=>{
-                        navData.navigation.toggleDrawer()
-                    }}
-                />
-            </HeaderButtons>
-        )
-    }    
+FavoritesScreen.navigationOptions = (navData) => {
+	return {
+		headerTitle: 'Your Favorites',
+		headerLeft: () => (
+			<HeaderButtons HeaderButtonComponent={HeaderButton}>
+				<Item
+					title='Menu'
+					iconName='ios-menu'
+					onPress={() => {
+						navData.navigation.toggleDrawer()
+					}}
+				/>
+			</HeaderButtons>
+		),
+	}
 }
 
-export default FavoritesScreen;
+const styles = StyleSheet.create({
+	content: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
+})
+
+export default FavoritesScreen
